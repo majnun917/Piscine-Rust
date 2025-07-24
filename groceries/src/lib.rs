4f6340625ebe7e686b10f18d323f96b67a303d75
ndieye <dieye.ndiasse17@gmail.com>
2025-07-24 16:03:23 +0100

    groceries done

---
+++ groceries/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn insert(vec: &mut Vec<String>, val: String) {
+    vec.push(val);
+}
+pub fn at_index(slice: &[String], index: usize) -> &str {
+    slice[index].as_str()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
