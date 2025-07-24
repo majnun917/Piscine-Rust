93304a229810bdda214100673d2fe07119a129ea
ndieye <dieye.ndiasse17@gmail.com>
2025-07-24 12:09:21 +0100

    temperature_conv done

--- temperature_conv/target/debug/deps/temperature_conv-9cabfc961f50c130.d
+++
@@ -1,5 +0,0 @@
-/home/ndieye/Desktop/Variables/temperature_conv/target/debug/deps/temperature_conv-9cabfc961f50c130: src/lib.rs
-
-/home/ndieye/Desktop/Variables/temperature_conv/target/debug/deps/temperature_conv-9cabfc961f50c130.d: src/lib.rs
-
-src/lib.rs:
