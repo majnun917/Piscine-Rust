2bcca479a631aa2dd2ac60d6008529f0df1d1b58
ndieye <dieye.ndiasse17@gmail.com>
2025-07-25 11:38:57 +0100

    tuples_refs

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
+
 pub struct Matrix (pub (i32, i32), pub (i32, i32));
 
  pub fn transpose(m: Matrix) -> Matrix {
---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Student (pub u32,pub String,pub String);
+
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> &str {
+    student.1.as_str()
+}
+
+pub fn last_name(student: &Student) -> &str {
+    student.2.as_str()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
