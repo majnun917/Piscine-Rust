6f716d84d1863865e9214dd162f705de276df48d
ndieye <dieye.ndiasse17@gmail.com>
2025-07-27 14:16:24 +0100

    arrays done

--- arrays/target/debug/deps/arrays-54ed62d4fa652a44.d
+++
@@ -1,7 +0,0 @@
-/home/ndieye/Desktop/Piscine-Rust/arrays/target/debug/deps/libarrays-54ed62d4fa652a44.rmeta: src/lib.rs
-
-/home/ndieye/Desktop/Piscine-Rust/arrays/target/debug/deps/libarrays-54ed62d4fa652a44.rlib: src/lib.rs
-
-/home/ndieye/Desktop/Piscine-Rust/arrays/target/debug/deps/arrays-54ed62d4fa652a44.d: src/lib.rs
-
-src/lib.rs:
--- arrays/target/debug/deps/arrays-90a1fbd21f84f44d.d
+++
@@ -1,5 +0,0 @@
-/home/ndieye/Desktop/Piscine-Rust/arrays/target/debug/deps/arrays-90a1fbd21f84f44d: src/lib.rs
-
-/home/ndieye/Desktop/Piscine-Rust/arrays/target/debug/deps/arrays-90a1fbd21f84f44d.d: src/lib.rs
-
-src/lib.rs:
