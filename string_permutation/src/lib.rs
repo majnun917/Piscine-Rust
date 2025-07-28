c2260512b80530ae3cc96621def1d974f12fc000
ndieye <dieye.ndiasse17@gmail.com>
2025-07-28 19:15:42 +0100

    string_permutation done

--- string_permutation/src/lib.rs
+++ string_permutation/src/lib.rs
@@ -1,8 +1,55 @@
 pub fn is_permutation(s1: &str, s2: &str) -> bool {
-    for c in s1.chars() {
-        if !s2.contains(c) {
-            return false;
-        }
+    if s1.chars().count() != s2.chars().count() {
+        return false;
     }
-    true
+    let mut s1_chars: Vec<char> = s1.chars().collect();
+    let mut s2_chars: Vec<char> = s2.chars().collect();
+
+    s1_chars.sort_unstable();
+    s2_chars.sort_unstable();
+
+    s1_chars == s2_chars
 }
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_basic() {
+//         assert!(is_permutation("abcde", "edbca"));
+//         assert!(!is_permutation("avcde", "edbca"));
+//         assert!(!is_permutation("cde", "edbca"));
+//         assert!(is_permutation("code", "deco"));
+//         assert!(!is_permutation("code", "deeco"));
+//         assert!(!is_permutation("codde", "deeco"));
+//     }
+
+//     #[test]
+//     fn test_repeating_characters() {
+//         assert!(is_permutation("aab", "baa"));
+//     }
+
+//     #[test]
+//     fn test_one_char() {
+//         assert!(!is_permutation("a", "b"));
+//         assert!(is_permutation("a", "a"));
+//     }
+
+//     #[test]
+//     fn test_empty() {
+//         assert!(is_permutation("", ""));
+//     }
+
+//     #[test]
+//     fn test_special_characters() {
+//         assert!(is_permutation("!#%@", "@%#!"));
+//     }
+
+//     #[test]
+//     fn test_unicode() {
+//         assert!(is_permutation("hello♥", "♥oelhl"));
+//         assert!(!is_permutation("♥", "♥♥"));
+//     }
+// }
