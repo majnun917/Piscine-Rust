d7acb3f86d583553c10afd3e1af10f20b5a9e4e2
ndieye <dieye.ndiasse17@gmail.com>
2025-08-01 21:57:43 +0100

    question_mark done

--- profanity_filter/src/lib.rs
+++ profanity_filter/src/lib.rs
@@ -1,8 +1,8 @@
 // Empty file
 pub fn check_ms(message: &str) -> Result<&str, &str> {
-      if message.contains("stupid") || message.is_empty(){
+    if message.contains("stupid") || message.is_empty() {
         Err("ERROR: illegal")
-      }else{
-                Ok(message)
-      }
+    } else {
+        Ok(message)
+    }
 }
---
+++ question_mark/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct One {
+    first_layer: Option<Two>
+}
+pub struct Two {
+    second_layer: Option<Three>
+}
+pub struct Three {
+    third_layer: Option<Four>
+}
+pub struct Four {
+  fourth_layer: Option<u16>
+}
+
+impl One {
+    pub fn get_fourth_layer(self) -> Option<u16> {
+       self.first_layer?.second_layer?.third_layer?.fourth_layer
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//    use crate::One;
+//     use crate::Two;
+//     use crate::Three;
+//     use crate::Four;
+// fn create_nested(value: Option<u16>) -> One {
+//     One {
+//         first_layer: Some(Two {
+//             second_layer: Some(Three {
+//                 third_layer: Some(Four {
+//                     fourth_layer: value,
+//                 }),
+//             }),
+//         }),
+//     }
+// }
+
+// #[test]
+// fn test_value() {
+//     assert_eq!(create_nested(Some(1000)).get_fourth_layer(), Some(1000));
+//     assert_eq!(create_nested(None).get_fourth_layer(), None);
+// }
+// }
